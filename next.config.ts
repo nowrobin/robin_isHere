4b5f8ec55b53426f6c0a2639ebd9505fc5709514
robin <nowrobin3@gmail.com>
2025-07-22 22:56:46 +0900

    refactor: add  header in config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/videos/(.*)', // public/videos 폴더 기준
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'video/webm',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
