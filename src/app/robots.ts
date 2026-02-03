2d6884679a11f058c695e5252f1585a658601d0f
robin <nowrobin3@gmail.com>
2026-02-03 19:59:29 +0900

    url added

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,8 +4,8 @@ const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_SITE_URL) {
     return process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, '');
   }
-  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return `${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
   }
   return 'http://localhost:3000';
 };
